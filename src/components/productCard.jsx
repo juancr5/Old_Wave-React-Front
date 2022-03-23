4ddf26602a117417255e8ead9a6b3d0b994d8562
1214741 <j98fer@gmail.com>
2022-03-22 22:30:01 -0500

    Quit boostrap and add Mui, card redesing

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,9 +1,14 @@
-import { Card, Button } from "react-bootstrap"
-import './productCard.css'
+import './ProductCard.css'
 import imageDefault from '../assets/images/product-default.png'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
 
 export default function ProductCard({
-     product : {product_code, name, brand, thumbnail, city, price, seller, rating, search_quantity}
+    product: { product_code, name, brand, thumbnail, city, price, seller, rating, search_quantity }
 }) {
 
     const formatNumberToPrice = (price) => {
@@ -12,7 +17,7 @@ export default function ProductCard({
     }
 
     const imageInCard = (thumbnail) => {
-        if(thumbnail != null && thumbnail !== ""){
+        if (thumbnail != null && thumbnail !== "") {
             return thumbnail
         }
         return imageDefault
@@ -20,19 +25,28 @@ export default function ProductCard({
 
     return (
         <>
-            <Card className="Card">
-                <Card.Img className="img" src={imageInCard(thumbnail)} alt="imagen" />
-                <Card.Body>
-                    <Card.Title className="title">{name}</Card.Title>
-                    <Card.Text className="brand">{brand}</Card.Text>
-                    <Card.Text className="price">
-                        {formatNumberToPrice(price)}
-                    </Card.Text>
-                    <Button className="button rounded-pill pt-0 pb-0" style={{ backgroundColor: '#772CE8', borderColor: 'transparent', fontSize: '14px', fontWeight: 'bold' }}>Agregar al Carrito</Button>
-                </Card.Body>
+            <Card className='Card'>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={imageInCard(thumbnail)}
+                    alt="Producto"
+                />
+                <CardContent>
+                    <Typography className='title'>
+                        {name}
+                    </Typography>
+                    <Typography className='brand'>
+                        {brand}
+                    </Typography>
+                    <Typography className='price'>
+                    {formatNumberToPrice(price)}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button className='button'>Agregar al Carrito</Button>
+                </CardActions>
             </Card>
         </>
     )
 }
-
-// style={{ backgroundColor: '#772CE8', borderColor: 'transparent', fontSize: '14px' }}
--- src/views/products/Products.jsx
+++ src/views/products/Products.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import ProductCard from '../../components/ProductCard'
 import { getProductsByName } from '../../services/ProductServices';
-import { Button } from 'react-bootstrap'
-
+import Grid from "@mui/material/Grid";
+import Button, { ButtonProps } from '@mui/material/Button';
 
 const Products = () => {
     const [productData, setProductData] = useState([]);
@@ -14,35 +14,21 @@ const Products = () => {
         setSearchValue(value)
     };
     const handleClick = (e) => {
-        console.log("iphone")
-        getProductsByName("iphone").then((product) => {
-            setProductData(product.data.results);
-            console.log(productData);
+        getProductsByName("", 1).then((product) => {
+            setProductData(product.data.items);
         });
     };
 
 
     return (
         <>
-            {/* <Button onClick={() => handleClick()}>Buscar</Button>
-            <ul>
-                {productData.map((item, i) => (
-                    // <ProductCard key={item.product_code} product={item} />
-                    <li key={i}>{item}</li>
+            <Button onClick={() => handleClick()}>Buscar</Button>
+            <Grid container spacing={2}>
+                {productData && productData.map((item) => (
+                    <ProductCard key={item.product_code} product={item} />
+                    // <li key={i}>{item}</li>
                 ))}
-            </ul> */}
-
-            <ProductCard product={{
-                "product_code": "1",
-                "name": "iPhone 13 128 GB",
-                "brand": "Apple",
-                "thumbnail": "https://firebasestorage.googleapis.com/v0/b/oldwave-fastapi-backend.appspot.com/o/product_thumbnails%2Fimage_2022-03-19_000715.png?alt=media&token=f9fd4202-a384-440f-9ae4-1c1ac2dba837",
-                "city": "Medellin",
-                "price": 70000,
-                "seller": "Ernesto Perez Frailejón Dev",
-                "rating": 5,
-                "search_quantity": 30
-            }} />
+            </Grid>
         </>
     )
 }
