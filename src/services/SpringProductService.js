81328483dc7f2e58aaa00a3eaddf13931b1143c7
juancr5 <juancr5@hotmail.com>
2022-04-09 05:38:27 -0500

    Update name variables

--- src/services/SpringProductService.js
+++ src/services/SpringProductService.js
@@ -4,13 +4,13 @@ const apiUrlSpring = 'https://oldwave-spring-api.herokuapp.com/api'
 
 //http://20.228.205.221/api/product/?page=1&size=50
 export const getProductsByName = async (name, page) => {
-    const resp = await axios.get(`${apiUrlSite}/product?name=${name}&page=${page}&size=20`);
+    const resp = await axios.get(`${apiUrlSpring}/product?name=${name}&page=${page}&size=20`);
     return resp;
 }
 
 //http://20.228.205.221/api/product/{id}/detail/
 export const getProductDesc = async (code) => {
-    const resp = await axios.get(`${apiUrlSite}/product/${code}/detail`);
+    const resp = await axios.get(`${apiUrlSpring}/product/${code}/detail`);
     return resp;
 }
 
