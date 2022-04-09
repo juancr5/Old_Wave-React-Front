aa0426b5febe6c5036c21bce6d2af65303ae5556
juancr5 <juancr5@hotmail.com>
2022-04-09 17:01:39 -0500

    Update ProductServices.js

--- src/services/ProductServices.js
+++ src/services/ProductServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrlSecond = 'http://ec2-3-84-235-83.compute-1.amazonaws.com/api'
+const apiUrlSecond = 'https://oldwave-fastapi-backend.herokuapp.com/api'
 
 //http://20.228.205.221/api/product/?page=1&size=50
 export const getProductsByName = async (name, page) => {
