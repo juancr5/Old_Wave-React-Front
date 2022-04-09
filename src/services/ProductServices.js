1e2becf2a2a72f4a30c016ce4ef7465f1bb99057
1214741 <j98fer@gmail.com>
2022-04-09 10:26:45 -0500

    urls

--- src/services/ProductServices.js
+++ src/services/ProductServices.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const apiUrlSecond = 'http://ec2-3-84-235-83.compute-1.amazonaws.com/api'
-const apiURLSpring = 'https://oldwave-spring-api.herokuapp.com/api/swagger-ui.html#'
-/** F por Cors los de Spring */
 
 //http://20.228.205.221/api/product/?page=1&size=50
 export const getProductsByName = async (name, page) => {
-    //const resp = await axios.get(`${apiUrlSecond}/product/?name=${name}&page=${page}&size=20`);
-    const resp = await axios.get(`${apiURLSpring}/product/?name=${name}&page=${page}&size=20/`)
+    const resp = await axios.get(`${apiUrlSecond}/product/?name=${name}&page=${page}&size=20`);
+    //const resp = await axios.get(`${apiURLSpring}/product/?name=${name}&page=${page}&size=20/`)
     return resp;
 }
 
